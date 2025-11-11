8aed713dae0f9b01be12bb2749b7fc789b0b8474
ROswald23 <106355130+ROswald23@users.noreply.github.com>
2025-11-11 21:09:40 +0100

    Update main.tsx

--- my-frontend/src/main.tsx
+++ my-frontend/src/main.tsx
@@ -1,18 +1,8 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import pp from './App.tsx'
-//
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./appli";
+import App from "./App.tsx";
 import "./index.css";
 
 
